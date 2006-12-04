770816928314db9e89ecb89c8e41fa8b77fabbef
whit537 <whit537@095d4182-ca23-0410-93eb-bd8822c811d7>
2006-12-04 19:13:58 +0000

    tagging initial release

---
+++ tags/1.0a1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+
+classifiers = [
+    'Development Status :: 3 - Alpha'
+  , 'Environment :: Console'
+  , 'Intended Audience :: Developers'
+  , 'License :: OSI Approved :: MIT License'
+  , 'Natural Language :: English'
+  , 'Operating System :: MacOS :: MacOS X'
+  , 'Operating System :: Microsoft :: Windows'
+  , 'Operating System :: POSIX'
+  , 'Programming Language :: Python'
+  , 'Topic :: Internet :: WWW/HTTP :: WSGI'
+  , 'Topic :: Software Development :: Libraries :: Python Modules'
+   ]
+
+setup( name = 'lib537'
+     , version = '~~VERSION~~'
+     , package_dir = {'':'src'}
+     , packages = ['lib537']
+     , description = 'lib537 is a Python library.'
+     , long_description = """\
+lib537 is a Python library bundling the following modules:
+
+  httpy -- smooth over WSGI's worst warts
+  mode -- manage the application life-cycle from debugging to production
+  restarter -- automatically restart your program when module files change
+"""
+     , author = 'Chad Whitacre'
+     , author_email = 'chad@zetaweb.com'
+     , url = 'http://www.zetadev.com/software/lib537/'
+     , classifiers = classifiers
+      )
